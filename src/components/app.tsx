32d8aab3b86b4e30456a934690c9de4d0707d580
danny <bahuko@gmail.com>
2022-03-20 16:37:51 +0100

    switch to typescript

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import "../styles/app.css";
+
+const App = () => (
+  <div className="App">
+    <header>
+      <h1 className="title">キーボード</h1>
+      <h2 className="subtitle">Kiiboodo</h2>
+    </header>
+  </div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./styles/main.css";
+
+import App from "./components/app";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
