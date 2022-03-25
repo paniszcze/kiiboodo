e913a3023ea797b7cd75bde777fc0e7eb872c91c
danny <bahuko@gmail.com>
2022-03-26 00:54:43 +0100

    replace some divs with a semantic tag

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,10 @@ import * as React from "react";
 import "../styles/header.css";
 
 const Header = () => (
-  <div className="Header">
-    <header>
-      <h1 className="title">キーボード</h1>
-      <h2 className="subtitle">Kiiboodo</h2>
-    </header>
-  </div>
+  <header>
+    <h1 className="title">キーボード</h1>
+    <h2 className="subtitle">Kiiboodo</h2>
+  </header>
 );
 
 export default Header;
