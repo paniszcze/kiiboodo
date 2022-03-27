575ed51ed7c39b110e7fb6605972025eba0a951c
danny <bahuko@gmail.com>
2022-03-27 11:45:48 +0200

    destructure input component

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -2,19 +2,39 @@ import * as React from "react";
 
 import "../styles/canvas.css";
 
-import Input from "./input";
+import { CanvasProps, WordInterface } from "../utils/types";
 
-const Canvas = () => {
-  const [words, setWords] = React.useState<string[]>([]);
+const Canvas = ({ stats: { score, lives }, setStats }: CanvasProps) => {
+  const [words, setWords] = React.useState<WordInterface[]>([]);
   const [userInput, setUserInput] = React.useState("");
 
-  const addWord = (word: string) => {
-    setWords((prevWords) => [...prevWords, word]);
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
   };
 
+  // addWord
+  // removeWord
+  // populateWords
+
   return (
-    <div className="Canvas pixel-border">
-      <Input userInput={userInput} setUserInput={setUserInput} />
+    <div className="Canvas">
+      {words.length !== 0 &&
+        words.map((word, index) => (
+          <div
+            key={index}
+            className="word"
+            style={{ top: `${word.y}px`, left: `${word.x}px` }}
+          >
+            {word.text}
+          </div>
+        ))}
+      <input
+        type="text"
+        name="text"
+        placeholder="..."
+        value={userInput}
+        onChange={handleInput}
+      />
     </div>
   );
 };
