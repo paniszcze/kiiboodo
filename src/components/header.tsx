09a5ca5ab3a02247900038acd7023a5150c53b3c
danny <bahuko@gmail.com>
2022-03-31 18:08:22 +0200

    fix romaji trancsription

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import "../styles/header.css";
 const Header = () => (
   <header>
     <h1 className="title" lang="ja">キーボード</h1>
-    <h2 className="subtitle">Kiiboodo</h2>
+    <h2 className="subtitle">Kībōdo</h2>
   </header>
 );
 
